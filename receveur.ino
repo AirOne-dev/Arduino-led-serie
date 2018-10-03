c47a2bfcbfb523a6990c92dd6d96c387d1521994
Firo <31392527+Firologame@users.noreply.github.com>
2018-10-03 10:58:50 +0200

    Add files via upload

---
+++ receveur.ino
@@ -0,0 +1,20 @@
+const int led = 3;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available()){
+    int msg = Serial.parseInt();
+    if (msg == 1) {
+      digitalWrite(led, HIGH);
+      Serial.println(2);
+    }
+    else {
+      digitalWrite(led, LOW);
+      Serial.println(3);
+    }
+  }
+}
---
+++ sender.ino
@@ -0,0 +1,30 @@
+const int button = 3;
+int etat_button = 0;
+int led = 13;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(button, INPUT);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  etat_button = digitalRead(button); //------------------------
+  if (etat_button == HIGH) {         //
+    Serial.println(1);               // Partie qui envoie '1'
+  }                                  // sur le port serie quand
+  else {                             // on appuie sur le boutton
+    Serial.println(0);               //  
+  }                                  //------------------------
+
+  if (Serial.available()) {          //------------------------
+    int rep = Serial.parseInt();     //
+    if (rep == 2) {                  //
+      digitalWrite(led, HIGH);       // Partie qui allume la
+    }                                // LED quand on reçoit
+    else {                           // '2' sur le port série
+      digitalWrite(led, LOW);        //
+    }                                //
+  }                                  //------------------------
+  delay(50);
+}
